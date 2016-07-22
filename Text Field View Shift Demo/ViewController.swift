d4fea55b7dc6c458cb37d5a6e75ea82d2a81d361
Jaemoon <jaemoon@jaemoonpark.com>
2016-07-22 01:15:55 -0400

    Initial Commit

---
+++ Text Field View Shift Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Text Field View Shift Demo
+//
+//  Created by Jaemoon Park on 7/22/16.
+//  Copyright © 2016 Jaemoon Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
