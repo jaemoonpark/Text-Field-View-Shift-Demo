2063e9136f291e2410c41363806821567ffb8229
Jaemoon <jaemoon@jaemoonpark.com>
2016-07-25 00:09:10 -0400

    add textfields, modify attributes via code

--- Text Field View Shift Demo/ViewController.swift
+++ Text Field View Shift Demo/ViewController.swift
@@ -9,8 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let myAttributes = [NSStrokeColorAttributeName: UIColor.blackColor(), NSStrokeWidthAttributeName: 4.0,NSForegroundColorAttributeName: UIColor.whiteColor(), NSFontAttributeName: UIFont.init(name: "HelveticaNeue-Bold", size: 24.0)!]
+    //NSFontAttributeName: UIFont.init(name: "Comic Sans", size: 17.0)
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
     override func viewDidLoad() {
+       
+        topTextField.defaultTextAttributes = myAttributes
+        bottomTextField.defaultTextAttributes = myAttributes
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
